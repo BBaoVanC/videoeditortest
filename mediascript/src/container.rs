32c505f201b5bfdecfea2a5196ca10f482abc814
BBaoVanC <bbaovanc@bbaovanc.com>
2024-11-16 20:53:50 -0600

    Idk

--- mediascript/src/container.rs
+++ mediascript/src/container.rs
@@ -1,9 +1,5 @@
-//! Objects for muxing and demuxing media containers
+//! Monads for muxing and demuxing media containers
 
-pub struct MkvContainer {
-}
-impl MkvContainer {
-    pub fn open(path: &str) -> Result<Self, !> {
-    }
-    pub fn get_video(id
+pub struct MediaFile {
+    path: String,
 }
