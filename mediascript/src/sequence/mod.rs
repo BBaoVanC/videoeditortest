b95fc86966636bf57bd0a589ee807448c81ba7b2
BBaoVanC <bbaovanc@bbaovanc.com>
2024-11-26 15:02:50 -0600

    WIP

--- mediascript/src/sequence/mod.rs
+++ mediascript/src/sequence/mod.rs
@@ -1,17 +1,38 @@
 use crate::TimeRational;
 use crate::Fraction;
 
-/// A fixed-length segment of multiplexed media
-pub trait ClipFixedLength {
+/// A fixed-length clip of multiplexed media that can be worked with in the justification system.
+pub trait Segment {
     fn length(&self) -> TimeRational;
 }
 
-pub struct ClipConstantVelocity<T: ClipFixedLength> {
-    clip: T,
-    velocity: Fraction,
+/// Methods to help actually render frames
+pub struct SelectFrame<T: Segment> {
+    segment: T,
+
 }
-impl<T: ClipFixedLength> ClipFixedLength for ClipConstantVelocity<T> {
-    fn length(&self) -> TimeRational {
-        TimeRational(self.clip.length().0 * self.velocity)
-    }
+impl SelectFrame {
 }
+
+//pub struct ClipConstantVelocity<T: Segment> {
+//    clip: T,
+//    velocity: Fraction,
+//}
+//impl<T: Segment> Segment for ClipConstantVelocity<T> {
+//    fn length(&self) -> TimeRational {
+//        TimeRational(self.clip.length().0 / self.velocity)
+//    }
+//}
+//
+//pub struct ClipLinearVelocity<T: Segment> {
+//    clip: T,
+//    /// slope of velocity line
+//    dvelocity,
+//    /// the initial velocity at the clip's origin
+//    velocity0,
+//}
+//impl<T: Segment> Segment for ClipLinearVelocity<T> {
+//    fn length(&self) -> TimeRational {
+//
+//    }
+//}
